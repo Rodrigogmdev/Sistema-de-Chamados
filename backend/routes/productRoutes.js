83a8cce0dc521f64b44a1e448fb0ae0761bde1f2
yatosz <igor.vidaleufrazio@gmail.com>
2025-05-10 19:11:13 -0300

    ajuste de conflitos

--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productCtrl = require('../controllers/productController');
-
-router.get('/', productCtrl.listProducts);
-router.get('/category/:category', productCtrl.findByCategory);
-
-module.exports = router;
--- backend/routes/reportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportCtrl = require('../controllers/reportController');
-
-router.get('/', reportCtrl.generateReport);
-
-module.exports = router;
