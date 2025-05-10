bd612adc7ec6ebf92c591c5f5b9715174beb851e
IgorVidalDev <igor.vidal.00000@gmail.com>
2025-05-10 20:00:22 -0300

    Delete frontend directory

--- frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom/client');
-var App = require('./App');
-ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null, React.createElement(App))
-);
