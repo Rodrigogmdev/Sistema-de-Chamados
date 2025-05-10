9f393384370e5dc1f87b57d9660c2e4751bc61a6
yatosz <igor.vidaleufrazio@gmail.com>
2025-05-10 19:03:03 -0300

    Resolvendo conflitos

--- src/pages/Reports.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReportView = require('../components/ReportView');
-function Reports() {
-  return React.createElement('div', null,
-    React.createElement(ReportView)
-  );
-}
-module.exports = Reports;
--- src/utils/localStorage.js
+++
@@ -1,7 +0,0 @@
-exports.saveToLocalStorage = function(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-exports.getFromLocalStorage = function(key) {
-  var item = localStorage.getItem(key);
-  return item ? JSON.parse(item) : null;
-};
